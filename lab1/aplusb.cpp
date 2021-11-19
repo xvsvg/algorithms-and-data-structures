47a9eb572efb81cf3b222206147154d67c3020a1
xvsvg <pobedotriples@gmail.com>
2021-11-20 01:03:00 +0300

    labs

---
+++ lab1/aplusb.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("aplusb.in");
+    ofstream out("aplusb.out");
+
+    int a,b;
+
+    in>>a>>b;
+    out<<a+b<<endl;
+
+    return 0;
+}
